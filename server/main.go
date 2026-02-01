39adfa593c864a7da705ed690a75fcaf5d2b17cd
Guilherme Pereira <guilospanck@protonmail.com>
2026-02-01 12:59:40 +0000

    Add signaling golang server

---
+++ server/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type MsgType string
+
+const (
+	Offer        MsgType = "offer"
+	Answer       MsgType = "answer"
+	IceCandidate MsgType = "ice_candidate"
+)
+
+type Message struct {
+	MsgType MsgType `json:"msgtype"`
+	Value   string  `json:"value"`
+}
+
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+type Hub struct {
+	clients    map[*Client]struct{}
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+	mutex      sync.RWMutex
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func newHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]struct{}),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.mutex.Lock()
+			h.clients[client] = struct{}{}
+			h.mutex.Unlock()
+			log.Printf("Client connected. Total clients: %d", len(h.clients))
+
+		case client := <-h.unregister:
+			h.mutex.Lock()
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+				log.Printf("Client disconnected. Total clients: %d", len(h.clients))
+			}
+			h.mutex.Unlock()
+
+		case message := <-h.broadcast:
+			h.mutex.RLock()
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+			h.mutex.RUnlock()
+		}
+	}
+}
+
+func (h *Hub) handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("WebSocket upgrade error: %v", err)
+		return
+	}
+
+	client := &Client{
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+
+	h.register <- client
+
+	go client.writePump()
+	go client.readPump(h)
+}
+
+func (c *Client) readPump(h *Hub) {
+	defer func() {
+		h.unregister <- c
+		c.conn.Close()
+	}()
+
+	for {
+		_, messageBytes, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("WebSocket error: %v", err)
+			}
+			break
+		}
+
+		var msg Message
+		if err := json.Unmarshal(messageBytes, &msg); err != nil {
+			log.Printf("JSON unmarshal error: %v", err)
+			continue
+		}
+
+		log.Printf("Received message: type=%s", msg.MsgType)
+
+		broadcastMsg, _ := json.Marshal(msg)
+		h.broadcast <- broadcastMsg
+	}
+}
+
+func (c *Client) writePump() {
+	defer c.conn.Close()
+
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			if err := c.conn.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Printf("Write error: %v", err)
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	hub := newHub()
+	go hub.run()
+
+	http.HandleFunc("/ws", hub.handleWebSocket)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("WebSocket signaling server is running. Connect to /ws"))
+	})
+
+	port := ":8080"
+	log.Printf("Signaling server starting on %s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
